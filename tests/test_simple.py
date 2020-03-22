90370b8d10b58a41565384671a2d76d9544bbb88
phen0menon <roland.ibr@yahoo.com>
2020-03-22 14:23:15 +0300

    Implement simple append test for Tree

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -6,7 +6,14 @@ from justree import Tree
 def test_default_usage():
     tpl = (1, [(2, []), (3, [])])
     tr = Tree(value=1, children=(Tree(value=2, children=()), Tree(value=3, children=()),))
-    assert Tree.from_tuple(tpl) == tr
+    assert tr == Tree.from_tuple(tpl)
+
+
+def test_tree_append():
+    tpl = (1, [(2, []), (4, [])])
+    tree = Tree(value=1, children=(Tree(value=2, children=()),))
+    tree.append(Tree(value=4, children=()))
+    assert tree == Tree.from_tuple(tpl)
 
 
 def test_freeze_assert():
