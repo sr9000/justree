581edff77e5f7d17e51b2d3aa475adbc6553e332
Rogonov Stepan <pirat-spy@yandex.ru>
2020-03-22 12:20:57 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+__version__ = '1.0.0'
+
+
+setup(
+    name="justree",
+    version=__version__,
+    url='https://github.com/sr9000/justree',
+    author='Rogonov Stepan Alekseevich',
+    author_email='rogonovstepan@gmail.com',
+    description='An idiomatic Python 3 implementation of tree structure.',
+    long_description='''This is an idiomatic tree data structure implementation in python.''',
+    license="MIT License",
+    packages=['justree'],
+    keywords=['data structure', 'tree', 'tools'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+   ],
+)
